2060ff81754890be7598b2ae95865fe9fae0b90d
AbdelI <homeworkoracle@protonmail.com>
2020-05-25 14:20:41 +0200

    javascript contains dupe,done

--- contains-dupes/contains_dupe.js
+++ contains-dupes/contains_dupe.js
@@ -1,3 +1,11 @@
-var containsDuplicate = function(nums) {
+var containsDuplicate = function (nums) {
+  if (nums.length === new Set(nums).size) {
+    return false;
+  } else {
+    return true;
+  }
+};
 
-}
+let numbers = [1, 2, 3, 4, 5, 5];
+
+console.log(containsDuplicate(numbers));
